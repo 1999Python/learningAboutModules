23e93865ab55fa0ba2ebeec7310b93c17faec222
1999Python <raadiyaharend6@gmail.com>
2023-07-19 07:42:33 +0200

    1st commit

---
+++ greet.js
@@ -0,0 +1,4 @@
+module.exports = function greet(name){
+    return "Hello, " + name;
+}
+//Note the module.exports= this is what we use to create a CommonJS module.
---
+++ greet_xola.js
@@ -0,0 +1,8 @@
+// import the greet module that is in the current folder using CommonJS
+import greet from './greet.js'
+
+const greetNew = new User('Raadiyah')
+
+// call the function & print the result on the screen
+
+console.log(greetNew)
